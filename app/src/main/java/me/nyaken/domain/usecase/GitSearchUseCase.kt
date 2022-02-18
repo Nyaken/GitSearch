f303683a7ecad24bcedfda2a8edaf049dcaac20f
Nayken <kognyak@gmail.com>
2022-02-19 03:28:00 +0900

    GitSearch-9 Search Repositories UseCase 추가

---
+++ app/src/main/java/me/nyaken/domain/usecase/GitSearchUseCase.kt
@@ -0,0 +1,21 @@
+package me.nyaken.domain.usecase
+
+import dagger.Reusable
+import me.nyaken.common.DEFAULT_ITEM_SIZE
+import me.nyaken.domain.repository.GitSearchRepository
+import javax.inject.Inject
+
+@Reusable
+class GitSearchUseCase @Inject constructor(
+    private val gitSearchRepository: GitSearchRepository
+){
+
+    operator fun invoke(
+        query: String,
+        sort: String,
+        order: String,
+        per_page: Int = DEFAULT_ITEM_SIZE,
+        page: Int,
+    ) = gitSearchRepository.searchGitRepositories(query, sort, order, per_page, page)
+
+}

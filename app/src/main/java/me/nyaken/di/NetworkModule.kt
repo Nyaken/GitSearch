13feadf656101002b623a10a56cc4b3b03dae39e
Nayken <kognyak@gmail.com>
2022-02-19 02:13:22 +0900

    GitSearch-4 retrofit setting

--- app/src/main/java/me/nyaken/di/NetworkModule.kt
+++ app/src/main/java/me/nyaken/di/NetworkModule.kt
@@ -1,11 +1,76 @@
 package me.nyaken.di
 
+import com.google.gson.Gson
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
 
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        interceptor: Interceptor
+    ): OkHttpClient {
+        val clientBuilder: OkHttpClient.Builder = OkHttpClient().newBuilder()
+            .connectTimeout(50, TimeUnit.SECONDS)
+            .readTimeout(50, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .addInterceptor(httpLoggingInterceptor)
+        return clientBuilder.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            setLevel(HttpLoggingInterceptor.Level.BODY)
+        }
+    }
+
+    @Provides
+    fun provideInterceptor(
+    ): Interceptor {
+        return Interceptor { chain ->
+            chain.proceed(chain.request().newBuilder().addHeader("Accept", "application/vnd.github.v3+json").build())
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient
+    ): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .baseUrl("https://api.github.com")
+            .client(okHttpClient)
+            .build()
+            .also {
+                retrofit = it
+            }
+    }
+
+    @Volatile
+    var retrofit: Retrofit? = null
+        private set
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return Gson()
+    }
 }
